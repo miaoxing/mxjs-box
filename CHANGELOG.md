a58905dcc50a4c6b5a586a5fca9a1f439d1e7b11
twinh <twinhuang@qq.com>
2020-07-31 15:08:46 +0800

    refactor: 更新包名和目录，避免前后端冲突

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# 0.1.0 (2020-07-30)
+
+
+### Features
+
+* 增加 box，同rebas，但是允许传值到子组件 ([](https://github.com/miaoxing/mxjs-box/commit/))
---
+++ index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import {
+  compose,
+  space,
+  layout,
+  typography,
+  color,
+  flexbox,
+} from 'styled-system'
+import css, {get} from '@styled-system/css'
+
+const sx = props => css(props.sx)(props.theme)
+const base = props => css(props.__css)(props.theme)
+const variant = ({theme, variant, tx = 'variants'}) => css(get(theme, tx + '.' + variant, get(theme, variant)))(theme)
+
+const invalidProps = ['as'];
+const regex = new RegExp("^(" + invalidProps.join('|') + ")$");
+
+export const Box = styled('div', {
+  shouldForwardProp: props => !regex.test(props),
+})(
+  base,
+  variant,
+  sx,
+  props => props.css,
+  compose(
+    space,
+    layout,
+    typography,
+    color,
+    flexbox,
+  ),
+)
+
+export const Flex = styled(Box)({
+  display: 'flex'
+})
