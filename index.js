95a3710d45ddce4e472f3aa0d4e99f1e1ee4a203
twinh <twinhuang@qq.com>
2020-08-14 17:23:35 +0800

    style(eslint): semi: always

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import styled from '@emotion/styled'
+import React from 'react';
+import styled from '@emotion/styled';
 import {
   compose,
   space,
@@ -7,12 +7,12 @@ import {
   typography,
   color,
   flexbox,
-} from 'styled-system'
-import css, {get} from '@styled-system/css'
+} from 'styled-system';
+import css, {get} from '@styled-system/css';
 
-const sx = props => css(props.sx)(props.theme)
-const base = props => css(props.__css)(props.theme)
-const variant = ({theme, variant, tx = 'variants'}) => css(get(theme, tx + '.' + variant, get(theme, variant)))(theme)
+const sx = props => css(props.sx)(props.theme);
+const base = props => css(props.__css)(props.theme);
+const variant = ({theme, variant, tx = 'variants'}) => css(get(theme, tx + '.' + variant, get(theme, variant)))(theme);
 
 const invalidProps = ['as'];
 const regex = new RegExp('^(' + invalidProps.join('|') + ')$');
@@ -31,8 +31,8 @@ export const Box = styled('div', {
     color,
     flexbox,
   ),
-)
+);
 
 export const Flex = styled(Box)({
   display: 'flex'
-})
+});
